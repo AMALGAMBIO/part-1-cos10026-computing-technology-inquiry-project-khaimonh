8079bb3ff777ddb380afffdaa2a6bfda00e7179b
nqrse <huyentrangno488@gmail.com>
2025-03-14 06:19:13 +0700

    project2 php

---
+++ project2/settings.php
@@ -0,0 +1,9 @@
+<?php
+// settings.php
+
+// Database connection variables
+$host = 'localhost';          // Replace with your database host if different
+$user = 'your_username';      // Replace with your MySQL username
+$password = 'your_password';  // Replace with your MySQL password
+$database = 'your_database_name';  // Replace with your database name
+?>
