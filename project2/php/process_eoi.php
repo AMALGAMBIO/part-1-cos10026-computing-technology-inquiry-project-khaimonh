c4235a5477e3be1a096b5ffab1b7b6df677c30f6
NguyenThiMaiNguyen <mainguyen020525@gmail.com>
2025-03-19 14:44:51 +0700

    handle cv upload

--- project2/php/process_eoi.php
+++ project2/php/process_eoi.php
@@ -42,43 +42,70 @@
         if (isset($_POST["jobnumber"]))
             $jobnumber = sanitise_input($_POST["jobnumber"]);	     //get Job Number
             echo "<b>Job reference number:</b> $jobnumber <br>";
+
         // Handle file upload 
-        if (isset($_POST['submit'])){
-            $file = $_FILES['file'];
-            print_r($file);
-            $fileName = $_FILES['file']['name'];
-            $fileTMPName = $_FILES['file']['tmp_name'];
-            $fileSize = $_FILES['file']['size'];
-            $fileError = $_FILES['file']['error'];
-            $fileType = $_FILES['file']['type'];
 
-            $fileExt = explode('.', $fileName);
-            $fileActualExt = strtolower(end($fileExt));
+// redirect to upload form if no file has been uploaded or an error occurred
+if (!isset($_FILES['fileToUpload']) || $_FILES['fileToUpload']['error'] > 0) {
+  header('Location: index.html');
+  exit;
+}
 
-            $allowed = array('jpg', 'jpeg', 'png', 'pdf');
+// configuration
+$uploadDirectory = getcwd() . '/uploads/';
+$fileExtensionsAllowed = ['jpeg', 'jpg', 'png']; // These will be the only file extensions allowed
+$mimeTypesAllowed = ['image/jpeg', 'image/png']; // These will be the only mime types allowed
+$maxFileSizeInBytes = 1024 * 1024 * 5; // 5 megabytes
 
-            if (in_array($fileActualExt, $allowed)) {
-               if($fileError === 0) {
-                 if ($fileSize < 6000000) {
-                    $fileNameNew = uniqid('',true).".".$fileActualExt;
-                    $fileDestination = '.uploads/'.$fileNameNew;
-                    move_uploaded_file($fileTMPName, $fileDestination);
-                    header("location: process_eoi.php?uploadsuccess");
-                 }
-                 else "You file is too big!<br>";
-               } 
-               else{
-                echo "There was an error uplpading your file!<br>";
-               }
-            }
-            else{
-                echo "You cannot upload files of this type!<br>";
-            }
-        } 
+// extract data about the uploaded file
+['name' => $fileName, 'tmp_name' => $fileTempName, 'type' => $fileType, 'size' => $fileSize] = $_FILES['fileToUpload'];
+['extension' => $fileExtension] = pathinfo($fileName);
+
+$errors = [];
+
+// validate the file extension is in our allow list
+if (!in_array($fileExtension, $fileExtensionsAllowed, strict: true)) {
+  $errors[] = 'File must have one of the following extensions: ' . implode(', ', $fileExtensionsAllowed) . '.';
+}
+
+// validate the file is an allowed mime type based on actual contents
+$detectedType = mime_content_type($fileTempName) ?: 'unknown type';
+if (!in_array($detectedType, $mimeTypesAllowed, strict: true)) {
+  $errors[] = 'File must have one of the following mime types: ' . implode(', ', $mimeTypesAllowed) . '.';
+}
+
+// validate if the file already exists
+$uploadPath = $uploadDirectory . $fileName;
+if (file_exists($uploadPath)) {
+  $errors[] = 'File with the same name already exists.';
+}
+
+// validate the maximum file size
+if ($fileSize > $maxFileSizeInBytes) {
+  $errors[] = 'File must not be greater than ' . number_format($maxFileSizeInBytes) . ' bytes.';
+}
+
+// verify for errors and move the file upon successful validation
+if (count($errors) > 0) {
+  echo '<h3>Errors</h3>';
+  echo '<ul>';
+  foreach ($errors as $error) {
+    echo '<li>' . $error . '</li>';
+  }
+  echo '</ul>';
+
+  exit;
+}
+
+if (move_uploaded_file($fileTempName, $uploadPath)) {
+  echo 'The file has been successfully uploaded. <br>';
+} else {
+  echo 'There was an error uploading your file. <br>';
+}
 
         if (isset($_POST["skills"]))
             $skills = sanitise_input($_POST["skills"]);	             //get Other Skills
-            echo "<b>Other skills:</b> $skills"
+            echo "<b>Other skills:</b> $skills";
 		
 	?>
 </body>
